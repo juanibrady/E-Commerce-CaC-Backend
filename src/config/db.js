c049f45328eea1bbfe03e1a697065c31d72a155a
juanibrady <juanibrady@gmail.com>
2024-07-12 16:42:11 -0300

    Agregando configuracion dotenv

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise"
-
+import "dotenv/config"
 export const pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    database:"crudluxperi",
+    host:process.env.HOST,
+    user:process.env.USER,
+    database:process.env.DB,
     waitForConnections:"true",
     connectionLimit:5
 })
